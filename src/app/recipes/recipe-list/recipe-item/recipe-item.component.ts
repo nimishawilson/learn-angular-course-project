fbd91c18e57124ed4e1007909433fbe0c9232824
Nimisha Wilson <nimishawilson123@gmail.com>
2020-01-05 12:13:35 +0530

    passong data with event and property binding

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
+// import { EventEmitter } from 'protractor';
 
 @Component({
   selector: 'app-recipe-item',
@@ -8,10 +9,12 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
 @Input() recipe: Recipe;
-
+@Output() recipeSelected = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit() {
   }
-
+  onSelected(){
+    this.recipeSelected.emit();
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,15 +8,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes:Recipe[] = [
     new Recipe('Meatball recipe', 'this is simply a test', 'https://www.cookingclassy.com/wp-content/uploads/2019/09/meatballs-21.jpg'),
-  new Recipe('Sri Lankan Chicken Curry', 'this is simply a test', 'https://www.theflavorbender.com/wp-content/uploads/2018/02/Sri-Lankan-Chicken-Curry-The-Flavor-Bender-Featured-Image-SQ-2-500x500.jpg'),
-    new Recipe('Strawberry syrup', 'this is simply a test', 'https://www.cookingclassy.com/wp-content/uploads/2018/04/strawberry-syrup-7-267x347.jpg')
+  new Recipe('Sri Lankan Chicken Curry', 'this is  a test', 'https://www.theflavorbender.com/wp-content/uploads/2018/02/Sri-Lankan-Chicken-Curry-The-Flavor-Bender-Featured-Image-SQ-2-500x500.jpg'),
+    new Recipe('Strawberry syrup', 'this istest', 'https://www.cookingclassy.com/wp-content/uploads/2018/04/strawberry-syrup-7-267x347.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe:Recipe){
+      this.recipeWasSelected.emit(recipe);
   }
 
 }
