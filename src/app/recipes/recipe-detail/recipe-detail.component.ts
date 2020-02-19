c9fefe08e6b41ab954ea29f45cc92b73cd07a8de
Nimisha Wilson <nimishawilson123@gmail.com>
2020-02-19 20:34:41 +0530

    adding an image preview

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -35,7 +35,7 @@ export class RecipeDetailComponent implements OnInit {
 
   onDeleteRecipe(){
     this.recipeService.deleteRecipe(this.id);
-    this.router.navigate(['/recipes'])
+    this.router.navigate(['/recipes']);
   }
 
 }
