18867e8bb60582e27e7307941ea46d53b88cc898
Nimisha Wilson <nimishawilson123@gmail.com>
2020-02-20 08:23:00 +0530

    storing recipes into firebase

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({providedIn:'root'})
 export class DataStorageService {
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient, private recipeService: RecipeService) {
 
+    }
+
+    storeRecipes(){
+        const recipes = this.recipeService.getRecipes();
+         this.http.put('https://recipesapp-2f70d.firebaseio.com/recipes.json', recipes).subscribe( response => { console.log(response); })
     }
  }
