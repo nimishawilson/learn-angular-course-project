5bc592143e8207c930611983fdd08488ce77c064
Nimisha Wilson <nimishawilson123@gmail.com>
2020-02-20 08:44:01 +0530

    fetching recipes from firebase

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable({providedIn:'root'})
 export class DataStorageService {
@@ -11,5 +12,12 @@ export class DataStorageService {
     storeRecipes(){
         const recipes = this.recipeService.getRecipes();
          this.http.put('https://recipesapp-2f70d.firebaseio.com/recipes.json', recipes).subscribe( response => { console.log(response); })
+    }
+
+    fetchRecipes(){
+        this.http.get<Recipe[]>('https://recipesapp-2f70d.firebaseio.com/recipes.json').subscribe(recipes => {
+            console.log(recipes);
+            this.recipeService.setRecipes(recipes);
+        })
     }
  }
