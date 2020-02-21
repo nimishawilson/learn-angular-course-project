30c3794a80a53f31603755db5ed7231876b52343
Nimisha Wilson <nimishawilson123@gmail.com>
2020-02-21 15:33:15 +0530

    add specific error display in error handling

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -31,9 +31,24 @@ export class AuthComponent {
          this.authService.signup(email,password).subscribe(resData => {
             console.log(resData);
             this.isLoading = false;
-        }, error => {
-            console.log(error);
-            this.error = ' An error occured';
+        }, errorRes => {
+            console.log(errorRes);
+            switch (errorRes.error.error.message) {
+                case 'EMAIL_EXISTS':
+                  this.error = 'this email id already exists';
+                break;
+                 case 'OPERATION_NOT_ALLOWED':
+                  this.error = 'Password sign-in is disabled for this project';
+                break;
+                 case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+                  this.error = 'We have blocked all requests from this device due to unusual activity. Try again later.';
+                break;
+            
+                default:
+                this.error = ' An error occured';
+                    break;
+            }
+            
             this.isLoading = false;
         });
 
