9862365c235ebc8999d2ac008abec06c989275a3
Nimisha Wilson <nimishawilson123@gmail.com>
2020-02-21 10:16:32 +0530

    prepare signup request

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+interface AuthResponseData {
+    kind: string,
+    idToken: string,
+    email: string,
+    refreshToken: string,
+    expiresIn: string,
+    localId: string
+}
+
+@Injectable({providedIn: 'root'})
+export class AuthService{
+
+    constructor(private http: HttpClient){ }
+
+    signup(email: string, password: string){
+       return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAA-EK1_6xPgCJpjR0x5dCastnZIsv0tPs'),
+        {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+    }
+
+}
