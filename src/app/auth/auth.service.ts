73bd0f397ba178dfdc444e1a9fffe4c13af08759
Nimisha Wilson <nimishawilson123@gmail.com>
2020-02-21 21:32:43 +0530

    login error handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { catchError } from "rxjs/operators";
 import { throwError } from "rxjs";
 
@@ -25,8 +25,22 @@ export class AuthService{
             password: password,
             returnSecureToken: true
         }
-        ).pipe(catchError(errorRes => {
-            let errorMessage = '';
+        ).pipe(catchError(this.handleError));
+       
+    }
+
+    login(email:string, password:string){
+       return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAA-EK1_6xPgCJpjR0x5dCastnZIsv0tPs',
+        {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        ).pipe(catchError(this.handleError));
+    }
+
+    private handleError(errorRes: HttpErrorResponse){
+         let errorMessage = '';
             if(!errorRes.error || !errorRes.error.error){
                 return throwError(errorMessage);
             }
@@ -40,6 +54,16 @@ export class AuthService{
                  case 'TOO_MANY_ATTEMPTS_TRY_LATER':
                   errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.';
                 break;
+
+                 case 'EMAIL_NOT_FOUND':
+                  errorMessage = 'There is no user record corresponding to this identifier. The user may have been deleted.';
+                break;
+                 case 'INVALID_PASSWORD':
+                  errorMessage = 'The password is invalid or the user does not have a password.';
+                break;
+                case 'USER_DISABLED':
+                  errorMessage = 'The user account has been disabled by an administrator.';
+                break;
             
                 default:
                 errorMessage = ' An error occured';
@@ -47,19 +71,6 @@ export class AuthService{
             }
 
              return throwError(errorMessage);
-             
-        }))
-       
-    }
-
-    login(email:string, password:string){
-       return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAA-EK1_6xPgCJpjR0x5dCastnZIsv0tPs',
-        {
-            email: email,
-            password: password,
-            returnSecureToken: true
-        }
-        )
     }
 
 }
