18f6e7f8099f0babbab804f34e6af8d92a8a92b3
Nimisha Wilson <nimishawilson123@gmail.com>
2020-02-25 19:06:55 +0530

    attaching the token with an interceptor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -17,18 +17,16 @@ export class DataStorageService {
     }
 
     fetchRecipes(){
-       return this.authService.user.pipe(take(1), exhaustMap(user => { 
-            return  this.http.get<Recipe[]>('https://recipesapp-2f70d.firebaseio.com/recipes.json', 
-            { 
-                params: new HttpParams().set('auth',user.token)
-             }
-            );
-        }),
-        map(recipes => {
+       
+            return  this.http.get<Recipe[]>('https://recipesapp-2f70d.firebaseio.com/recipes.json' 
+           
+            )
+        .pipe( map(recipes => {
             return recipes.map(recipe => {
-                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients: []}
+                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients: []};
             });
-        }),tap(recipes => {
+        }),
+       tap(recipes => {
             this.recipeService.setRecipes(recipes);
         })
         );
