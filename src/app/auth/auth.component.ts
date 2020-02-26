f3e804889ab76b54db0e0c729d876c9e76149def
Nimisha Wilson <nimishawilson123@gmail.com>
2020-02-26 13:03:42 +0530

    preparing programmatic creation

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,9 @@
-import { Component } from "@angular/core";
+import { Component, ComponentFactoryResolver } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { AuthService, AuthResponseData } from "./auth.service";
 import { Observable } from "rxjs";
 import { Router } from "@angular/router";
+import { AlertComponent } from "../shared/alert/alert.component";
 
 @Component({
     selector: 'app-auth',
@@ -12,8 +13,8 @@ export class AuthComponent {
     isLoginMode = true;
     isLoading = false;
     error:string = null;
-
-    constructor(private authService: AuthService, private router: Router){}
+errorMessage
+    constructor(private authService: AuthService, private router: Router, private componentFactoryResolver: ComponentFactoryResolver){}
 
     onSwitchMode(){
         this.isLoginMode = !this.isLoginMode;
@@ -40,6 +41,7 @@ export class AuthComponent {
         }, errorMessage => {
             console.log(errorMessage);
             this.error = errorMessage;
+            this.showErrorAlert(errorMessage);
             this.isLoading = false;
         });
 
@@ -49,5 +51,10 @@ export class AuthComponent {
 
     onHandleError(){
         this.error = null;
+    }
+
+    private showErrorAlert(message:string){
+        //const alertCmp = new AlertComponent();
+       const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
     }
 }
